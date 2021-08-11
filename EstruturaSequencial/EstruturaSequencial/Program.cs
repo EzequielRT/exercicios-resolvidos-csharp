441771ee835de649c94649d8d9c3a169eea71f0b
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-10 21:49:41 -0300

    Estrutura Sequencial - Ex_01

--- EstruturaSequencial/EstruturaSequencial/Program.cs
+++ EstruturaSequencial/EstruturaSequencial/Program.cs
@@ -6,7 +6,23 @@ namespace EstruturaSequencial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ResolucaoExercicio1();
         }
+
+
+        #region
+        //------------------------------------ Exercicio 1 ------------------------------------
+        //Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma 
+        //mensagem explicativa, conforme exemplos.
+        static void ResolucaoExercicio1()
+        {
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int soma = n1 + n2;
+            Console.WriteLine($"SOMA = {soma}");
+        }
+        #endregion
+
     }
+
 }
