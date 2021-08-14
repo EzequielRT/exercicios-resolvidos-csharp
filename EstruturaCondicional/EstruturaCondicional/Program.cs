7d711cc1a5f490e3f1d0c4b6c4ece1681f78c7fa
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-13 21:11:42 -0300

    Estrutura Condicional - Ex_01

---
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResolucaoExercicio1();
+        }
+
+        #region Exercicio 1
+        //------------------------------------ Exercicio 1 ------------------------------------
+        //fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+        static void ResolucaoExercicio1()
+        {
+
+            Console.Write("Digite um número para saber se é par ou ímpar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+        #endregion
+    }
+}
