0eb99a34462e1f55f4a28a6acf732878cc05e3dc
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-13 23:20:25 -0300

    Estrutura Condicional - Ex_06

--- EstruturaCondicional/EstruturaCondicional/Program.cs
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -11,7 +11,8 @@ namespace EstruturaCondicional
             //ResolucaoExercicio2();
             //ResolucaoExercicio3();
             //ResolucaoExercicio4();
-            ResolucaoExercicio5();
+            //ResolucaoExercicio5();
+            ResolucaoExercicio6();
         }
 
         #region Exercicio 1
@@ -162,6 +163,39 @@ namespace EstruturaCondicional
             }
 
             Console.WriteLine($"Total: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        #endregion
+
+        #region Exercicio 6
+        //------------------------------------ Exercicio 6 ------------------------------------
+        //Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+        //seguintes intervalos([0,25], (25,50], (50,75], (75,100]) este valor se encontra.Obviamente se o valor não estiver em
+        //nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+        static void ResolucaoExercicio6()
+        {
+            Console.Write("Digite um valor: ");
+            double numero = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero < 0.0 || numero > 100.0)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (numero <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (numero <= 50.0)
+            {
+                Console.WriteLine("Intervalo [25,50]");
+            }
+            else if (numero <= 75.0)
+            {
+                Console.WriteLine("Intervalo [50,75]");
+            }
+            else
+            {
+                Console.WriteLine("Intervalo [75,100]");
+            }
         }
         #endregion
     }
