fb111fa8fe83bcba5f73ca71f6cce4a3f8079a64
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-13 21:18:05 -0300

    Estrutura Condicional - Ex_02

--- EstruturaCondicional/EstruturaCondicional/Program.cs
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -6,7 +6,8 @@ namespace EstruturaCondicional
     {
         static void Main(string[] args)
         {
-            ResolucaoExercicio1();
+            //ResolucaoExercicio1();
+            ResolucaoExercicio2();
         }
 
         #region Exercicio 1
@@ -25,6 +26,26 @@ namespace EstruturaCondicional
             else
             {
                 Console.WriteLine("IMPAR");
+            }
+        }
+        #endregion
+
+        #region Exercicio 2
+        //------------------------------------ Exercicio 2 ------------------------------------
+        //Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+        static void ResolucaoExercicio2()
+        {
+
+            Console.Write("Digite um número para saber se é negativo ou não: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NAO NEGATIVO");
             }
         }
         #endregion
