12d702bcc3f622fe7f5f51320022edbbb1272d30
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-16 07:38:47 -0300

    Estrutura For - Ex_01

---
+++ EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResolucaoExercicio1();
+        }
+
+        #region Exercicio 1
+        //------------------------------------ Exercicio 1 ------------------------------------
+        //Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o 
+        //X, se for o caso.
+        static void ResolucaoExercicio1()
+        {
+            Console.Write("Digite um número: ");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= X; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        #endregion
+    }
+}
