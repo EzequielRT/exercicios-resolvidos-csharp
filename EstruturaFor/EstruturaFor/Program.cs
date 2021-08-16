0eb1c72ab40651c73bf0244ddbc41971dccd3910
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-16 08:19:06 -0300

    Estrutura For - Ex_03

--- EstruturaFor/EstruturaFor/Program.cs
+++ EstruturaFor/EstruturaFor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace EstruturaFor
 {
@@ -7,7 +8,8 @@ namespace EstruturaFor
         static void Main(string[] args)
         {
             //ResolucaoExercicio1();
-            ResolucaoExercicio2();
+            //ResolucaoExercicio2();
+            ResolucaoExercicio3();
         }
 
         #region Exercicio 1
@@ -57,6 +59,31 @@ namespace EstruturaFor
 
             Console.WriteLine($"{count_in} in");
             Console.WriteLine($"{count_out} out");
+        }
+        #endregion
+
+        #region Exercicio 3
+        //------------------------------------ Exercicio 3 ------------------------------------
+        //Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste 
+        //de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+        //conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5
+        static void ResolucaoExercicio3()
+        {
+            Console.Write("Digite a qtd de testes que deseja: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Digite os 3 valores que deseja saber a média ponderada:");
+                string[] valores = Console.ReadLine().Split(" ");
+                double n1 = Convert.ToDouble(valores[0], CultureInfo.InvariantCulture);
+                double n2 = Convert.ToDouble(valores[1], CultureInfo.InvariantCulture);
+                double n3 = Convert.ToDouble(valores[2], CultureInfo.InvariantCulture);
+
+                double mediaPonderada = (n1 * 2.0 + n2 * 3.0 + n3 * 5.0) / 10.0;
+
+                Console.WriteLine(mediaPonderada.ToString("F1", CultureInfo.InvariantCulture));
+            }
         }
         #endregion
     }
