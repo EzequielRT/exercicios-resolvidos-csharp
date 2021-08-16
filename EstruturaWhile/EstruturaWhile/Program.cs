28338f55e3192a690f1ae8bd4625b245510ae7a3
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-15 21:48:55 -0300

    Estrutura While - Ex_02

--- EstruturaWhile/EstruturaWhile/Program.cs
+++ EstruturaWhile/EstruturaWhile/Program.cs
@@ -6,7 +6,8 @@ namespace EstruturaWhile
     {
         static void Main(string[] args)
         {
-            ResolucaoExercicio1();
+            //ResolucaoExercicio1();
+            ResolucaoExercicio2();
         }
 
         #region Exercicio 1
@@ -28,6 +29,46 @@ namespace EstruturaWhile
             }
 
             Console.WriteLine("Acesso Permitido");
+        }
+        #endregion
+
+        #region Exercicio 2
+        //------------------------------------ Exercicio 2 ------------------------------------
+       //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema 
+       //cartesiano.Para cada ponto escrever o quadrante a que ele pertence.O algoritmo será encerrado quando pelo
+       //menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+
+        static void ResolucaoExercicio2()
+        {
+            Console.Write("Digite as coordenadas X e Y de pontos do sistema cartesiano (Ex: 2 2): ");
+            string[] pontos = Console.ReadLine().Split(" ");
+            int X = Convert.ToInt32(pontos[0]);
+            int Y = Convert.ToInt32(pontos[1]);
+
+            while (X != 0 && Y != 0)
+            {
+                if (X > 0 && Y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else if (X < 0 && Y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                }
+                else if (X < 0 && Y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto");
+                }
+
+                Console.Write("Digite as coordenadas X e Y de pontos do sistema cartesiano (Ex: 2 2): ");
+                pontos = Console.ReadLine().Split(" ");
+                X = Convert.ToInt32(pontos[0]);
+                Y = Convert.ToInt32(pontos[1]);
+            }
         }
         #endregion
     }
